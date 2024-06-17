8d26c7560ebe14f72ee9240dcad5f1beddf09896
girish332 <kulkarni.gi@northeastern.edu>
2024-06-17 14:49:39 -0400

    feat: created vpc for eks

---
+++ vpc.tf
@@ -0,0 +1,59 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+
+  name = "csye7125-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+  }
+}
+
+resource "aws_security_group" "eks_sg" {
+  description = "Kubernetes Security Group"
+  vpc_id      = module.vpc.vpc_id
+
+  // Allow all traffic within the security group for inter-node communication
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+  }
+
+  // Allow outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // Allow inbound traffic from EKS control plane
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // Allow inbound traffic from S3
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["52.216.0.0/15"]
+  }
+
+  tags = {
+    Name = "eks_sg"
+  }
+}
