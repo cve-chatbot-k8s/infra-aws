db983a6387cc8d1efe356ad60661928b837104b3
raazadarsh <raj.ada@northeastern.edu>
2024-06-24 19:18:36 -0400

    fix: fixed terraform fmt check

--- main.tf
+++ main.tf
@@ -18,21 +18,21 @@ module "vpc" {
 }
 
 module "iam" {
-  source                = "./iam"
+  source = "./iam"
   # worker_iam_role_names = module.eks.worker_iam_role_names
-  irsa_output           = module.eks.irsa_output
-  irsa_role_arn         = module.eks.irsa_role_arn
+  irsa_output   = module.eks.irsa_output
+  irsa_role_arn = module.eks.irsa_role_arn
 }
 
 module "eks" {
-  source                                 = "./eks"
-  vpc_id                                 = module.vpc.vpc_id
-  private_subnets                        = module.vpc.vpc_private_subnets
-  public_subnets                         = module.vpc.vpc_public_subnets
-  eks_ebs_encryption_key_arn             = module.iam.eks_ebs_encryption_key_arn
-  eks_secrets_encryption_key_arn         = module.iam.eks_secrets_encryption_key_arn
-  eks_cluster_role_arn                   = module.iam.eks_cluster_role_arn
-  worker_node_ebs_policy_arn             = module.iam.worker_node_ebs_policy_arn
+  source                         = "./eks"
+  vpc_id                         = module.vpc.vpc_id
+  private_subnets                = module.vpc.vpc_private_subnets
+  public_subnets                 = module.vpc.vpc_public_subnets
+  eks_ebs_encryption_key_arn     = module.iam.eks_ebs_encryption_key_arn
+  eks_secrets_encryption_key_arn = module.iam.eks_secrets_encryption_key_arn
+  eks_cluster_role_arn           = module.iam.eks_cluster_role_arn
+  worker_node_ebs_policy_arn     = module.iam.worker_node_ebs_policy_arn
 }
 
 
